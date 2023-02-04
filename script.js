eea2e9644109e27484e2169d1dfb9077c55e741f
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-02-04 18:49:41 +0100

    Add jump animation

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-const catImage = document.getElementById("cat");
-catImage.src = "/styles/1.1.png";
+// Cat Images array
+const catImages = ["/cat/1.png", "/cat/2.png", "/cat/3.png", "/cat/4.png", "/cat5.png", "/cat/6.png", "/cat/7.png", "/cat/8.png"];
+const cat = document.getElementById("cat");
+let currentImageIndex = 7;
 
-setTimeout(function () {
-    catImage.src = "/styles/2.2.png";
-}, 100);
-
-
-
-
-
-
+cat.src = catImages[currentImageIndex];
 
 
 
@@ -21,3 +15,41 @@ meowButton.addEventListener("click", function() {
     meowSound.currentTime = 0;
     meowSound.play();
 });
+
+document.getElementById("meow-button").addEventListener("click", function () {
+    let currentImageIndex = 1;
+    cat.src = catImages[currentImageIndex];
+
+    setTimeout(function () {
+        currentImageIndex = 7
+        cat.src = catImages[currentImageIndex];
+    }, 1000);
+});
+
+// Add feed effect
+document.getElementById("feed-button").addEventListener("click", function (){
+    
+})
+
+// Add tail effect
+document.getElementById("tail-button").addEventListener("click", function(){
+
+});
+
+// Add jump effect
+document.getElementById("jump-button").addEventListener("click", function (){
+    const catImage = document.getElementById("cat");
+
+    const jumpAnimation = anime({
+        targets: catImage,
+        translateY: [-100, 0],
+        easing: "easeOutQuad",
+        duration: 500,
+        loop: false
+    });
+    jumpAnimation.play();
+    
+});
+
+
+
